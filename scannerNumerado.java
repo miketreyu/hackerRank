7b63f5c60258289d0e51c15ddf655da79c161659
Mike <126797494+miketreyu@users.noreply.github.com>
2025-03-12 16:23:32 +0100

    update

---
+++ scannerNumerado.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+            while (sc.hasNext()) {  
+            String data = sc.nextLine();
+            count++;
+            System.out.println(count + " " + data);
+        }
+    }
+}
